ae7c7f3ef0418f064e885d77bb69f26bcaf623ee
Leandro Jacomelli <leandro.jacomelli@gmail.com>
2025-01-16 19:07:24 +0000

    fixing typo

--- start.sh
+++ start.sh
@@ -82,7 +82,7 @@ https://mail.{\$NC_DOMAIN}:443 {
         }
     }
 }
-https://autoconfig.{\$NC_DOMAIN}.fr:443 {
+https://autoconfig.{\$NC_DOMAIN}:443 {
     route /mail/config-v1.1.xml {
         reverse_proxy nextcloud-aio-stalwart:10003
     }
@@ -97,7 +97,7 @@ https://autoconfig.{\$NC_DOMAIN}.fr:443 {
         }
     }
 }
-https://autodiscover.{\$NC_DOMAIN}.fr:443 {
+https://autodiscover.{\$NC_DOMAIN}:443 {
     route /autodiscover/autodiscover.xml {
         reverse_proxy nextcloud-aio-stalwart:10003
     }
