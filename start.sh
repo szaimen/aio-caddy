40c5c16ac3a1fccdc635ba1044ecbdc445caf46a
Ruben D. <ruben@winterrific.net>
2025-11-23 20:15:59 +0100

    Add proxy protocol to TCP turn
    Signed-off-by: Ruben D <ruben@winterrific.net>

--- start.sh
+++ start.sh
@@ -265,7 +265,10 @@ if [ -n "$(dig A +short nextcloud-aio-talk)" ] && ! grep -q nextcloud-aio-talk /
           layer4 {
                      @turn not tls
                      route @turn {
-                                     proxy nextcloud-aio-talk:443
+                                     proxy {
+                                            proxy_protocol v1
+                                            upstream nextcloud-aio-talk:443
+                                      }
                                  }
                      route
                  }
